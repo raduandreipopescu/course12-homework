525f1f224a3a91704cdd22c5be28c07a96de7a62
raduandreipopescu <popescu.a.radu@gmail.com>
2023-03-06 19:19:07 +0200

    Cleared the reference escapes in DaySchedule & DailyPlanner

--- src/exerciseDailyPlanner/Day.java
+++ src/exerciseDailyPlanner/Day.java
@@ -1,5 +1,7 @@
 package exerciseDailyPlanner;
 
+import java.time.DayOfWeek;
+
 public enum Day {
     MONDAY("monday"),
     TUESDAY("tuesday"),
@@ -19,6 +21,8 @@ public enum Day {
         this.day = day;
     }
 
+//    DayOfWeek
+//    Asta este un enum similar celui facut de noi (din java.time.*)
 }
 
 
